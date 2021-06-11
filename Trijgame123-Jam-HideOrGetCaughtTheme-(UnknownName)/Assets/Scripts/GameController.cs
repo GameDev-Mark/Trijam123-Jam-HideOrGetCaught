13cc606601167f8fd813152e169a92fa7f484191
GameDev-Mark <gamedev.markd@gmail.com>
2021-06-11 17:18:37 +0200

    Script 08

--- Trijgame123-Jam-HideOrGetCaughtTheme-(UnknownName)/Assets/Scripts/GameController.cs
+++ Trijgame123-Jam-HideOrGetCaughtTheme-(UnknownName)/Assets/Scripts/GameController.cs
@@ -4,22 +4,33 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    [Space(20)]
     public Transform[] playerStartSpawn; // player starting position
 
-    public GameObject player; // player itself
-    public GameObject nextLvlButton; // button for next level
+    [Space(20)]
     public GameObject[] levels; // array of the levels
+
+    [Header("")]
+    public GameObject player; // player itself
+    [Space(10)]
+    public GameObject nextLvlButton; // button for next level
+    [Space(10)]
     public GameObject restartUI; // restart buton/text UI
+
+    [Header("")]
     public GameObject levelOneText; // texts for level 1
     public GameObject levelTwoText; // texts for level 2
+    public GameObject levelThreeText; // texts for level 3
+    public GameObject levelFourText; // texts for level 4
 
+    [Space(10)]
     public TMP_Text levelCountText; // level text UI
 
     float levelCounter; // counts the levels
     float timerStopTimeScale; // timer countdown for stopping timeScale (slow mo)
     float timerTurnOffTexts; // timer for UI texts
 
-    bool _playerCanSpawn; // if false, player can move from new start position
+    bool _playerCanSpawn = true; // if false, player can move from new start position
 
     // unitys start function
     void Start()
@@ -27,9 +38,6 @@ public class GameController : MonoBehaviour
         levelCounter = 1f;
         timerStopTimeScale = 0.3f;
         timerTurnOffTexts = 3f;
-
-        levelOneText.SetActive(true); // activate level 1 text
-        player.transform.position = playerStartSpawn[0].position; // spawn player at the start
     }
 
     // unitys update function
@@ -53,7 +61,13 @@ public class GameController : MonoBehaviour
         }
 
         // -------
-        if(levelCounter == 2 && _playerCanSpawn == true) // lvl 2
+        if(levelCounter == 1f && _playerCanSpawn == true)
+        {
+            levelOneText.SetActive(true); // activate level 1 text
+            player.transform.position = playerStartSpawn[0].position; // spawn player at the start
+            _playerCanSpawn = false;
+        }
+        if (levelCounter == 2f && _playerCanSpawn == true) // lvl 2
         {
             player.transform.position = playerStartSpawn[1].position; // new player position
             levelTwoText.SetActive(true);
@@ -61,23 +75,25 @@ public class GameController : MonoBehaviour
             levels[1].SetActive(true); // spawn level 2
             _playerCanSpawn = false; // if false, player can move from new start position
         }
-        if(levelCounter == 3 && _playerCanSpawn == true) // lvl 3
+        if(levelCounter == 3f && _playerCanSpawn == true) // lvl 3
         {
             player.transform.position = playerStartSpawn[2].position; // new player position
+            levelThreeText.SetActive(true);
             levels[1].SetActive(false); // despawn level 2
             levels[2].SetActive(true); // spawn level 3
             _playerCanSpawn = false; // if false, player can move from new start position
         }
-        if (levelCounter == 4 && _playerCanSpawn == true) // lvl 4
+        if (levelCounter == 4f && _playerCanSpawn == true) // lvl 4
         {
             player.transform.position = playerStartSpawn[4].position; // new player position
+            levelFourText.SetActive(true);
             levels[2].SetActive(false); // despawn level 3
             levels[3].SetActive(true); // spawn level 4
             _playerCanSpawn = false; // if false, player can move from new start position
         }
 
         // ------
-        if(levelOneText.activeInHierarchy || levelTwoText.activeInHierarchy)
+        if(levelOneText.activeInHierarchy || levelTwoText.activeInHierarchy || levelThreeText.activeInHierarchy || levelFourText.activeInHierarchy)
         {
             timerTurnOffTexts -= Time.deltaTime;
         }
@@ -86,6 +102,8 @@ public class GameController : MonoBehaviour
             timerTurnOffTexts = 3f;
             levelOneText.SetActive(false);
             levelTwoText.SetActive(false);
+            levelThreeText.SetActive(false);
+            levelFourText.SetActive(false);
         }
     }
 
